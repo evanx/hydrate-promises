34f74bc6441aac6a052f42cf6a7f90ef86449fdf
Evan Summers <evans@saratoga.co.za>
2015-03-24 07:44:53 +0200

    resync from jsbin

--- HydrateFromPromisesMixin.js
+++ HydrateFromPromisesMixin.js
@@ -2,27 +2,43 @@
 var debug = function() {   
 };
 
-var HydrateFromPromisesMixin = {
-   hydrateFromPromises: function(promises) {
-      debug('hydrate', Object.keys(promises));
-      let that = this;
-      let count = 0;
-      function set(key, data) {
-         that.state[key] = data;
-         count += 1;
-         if (count === Object.keys(promises).length) {
-            debug('hydrate resolved');
-            that.setState(that.state);
-         }
+function CountDownLatch(counter, then) {
+   this.signal = err => {
+      if (counter > 0) {
+         counter--;
       }
+      if (err) {
+         this.err = err;
+      }
+      if (counter === 0) {
+         then(this.err);
+      }
+   }
+}
+
+var hydrateFromPromisesMixin = {
+   hydrateFromPromises: function(promises, callback) {
+      log.debug('hydrate', Object.keys(promises));
+      let countDownLatch = new CountDownLatch(Object.keys(promises).length, err => {
+         this.setState(this.state);
+         if (callback) {
+           callback(err); 
+         }
+      });
       Object.keys(promises).forEach(key => {
-         promises[key]().then(data => {
-            debug('hydrate promise resolved', key);
-            set(key, data);
-         }, function(error) {
-            debug('hydrate promise rejected', key, error);
-            set(key, null);
-         });
+         try {
+            promises[key]().then(data => {
+               log.debug('hydrate promise resolved', key);
+               this.state[key] = data;
+               countDownLatch.signal();
+            }, error => {
+               log.debug('hydrate promise rejected', key, error);
+               countDownLatch.signal(error);
+            });
+         } catch (error) {
+            log.debug('hydrate promise exception', key, error);
+            countDownLatch.signal(error);  
+         }
       });
    }
 };
